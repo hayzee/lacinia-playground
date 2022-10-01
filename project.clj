eca51983dc62c93bf359a7a4e395d3e08fb307c3
hayzee <hayzee@users.noreply.github.com>
2022-10-01 19:39:56 +0100

    Initialised project.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.walmartlabs/lacinia "0.30.0"]
+                 [com.walmartlabs/lacinia-pedestal "0.5.0"]
+                 [io.aviso/logging "0.2.0"]]
   :repl-options {:init-ns lacinia-playground.core})
